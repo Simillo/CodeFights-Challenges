6bc0d62251ea18af7a1f954bc58fbcb981e11e39
Simillo <simillonakai@gmail.com>
2016-06-21 23:46:42 -0300

    Mudança

--- RPN (reverse polish notation).js
+++ RPN (reverse polish notation).js
@@ -3,8 +3,8 @@ RPN=q=>{
     r=q.split(" ")
     o="+-*/%^"
     s=[]
-    for (i in r)
-        w=r[i],
+    for (i of r)
+        w=i,
         ~o.indexOf(w)?(p=s.length-2,u=w=="^"?"Math.pow("+s[p]+","+s[p+1]+")":s[p]+w+s[p+1],s.pop(),s.pop(),s.push(eval(u))):s.push(w)
     return ~~s[0]
 }
