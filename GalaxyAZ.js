46aa61ec8c60182edd0bee646b45c331bc82bb87
Simillo <simillonakai@gmail.com>
2016-06-26 19:24:51 -0300

    119 chars

--- GalaxyAZ.js
+++ GalaxyAZ.js
@@ -2,6 +2,11 @@
 q=''
 GalaxyAZ=(n,b)=>
 	eval(`
-		for(i of n.toString(91-b.charCodeAt()).split(''))
+		for(i of n.toString(91-b.charCodeAt()))
 			q+=i>'_'?String.fromCharCode(90-(i.charCodeAt()-97)):i
 	`)
+
+
+/*Deveria funcionar. Não sei porque não funciona...
+ *n.toString(91-b.charCodeAt()).replace(/[a-z]/,a=>String.fromCharCode(90-(a.charCodeAt()-97)))
+ */
