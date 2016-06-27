e940ce6340487a807728541c129f4403bc6dd9c2
Simillo <simillonakai@gmail.com>
2016-06-27 11:41:19 -0300

    fix

--- GalaxyAZ.js
+++ GalaxyAZ.js
@@ -1,12 +1,14 @@
 //https://codefights.com/challenge/a9NhJYdzoBBPnxMJs/main
-q=''
-GalaxyAZ=(n,b)=>
+
+/*Deveria funcionar. Não sei porque não funciona...
+ *Agora sei!
+ */
+GalaxyAZ=(n,b)=>n.toString(91-b.charCodeAt()).replace(/[a-z]/g,a=>String.fromCharCode(90-(a.charCodeAt()-97)))
+
+/*GalaxyAZ=(n,b)=>
 	eval(`
+		q=''
 		for(i of n.toString(91-b.charCodeAt()))
 			q+=i>'_'?String.fromCharCode(90-(i.charCodeAt()-97)):i
 	`)
-
-
-/*Deveria funcionar. Não sei porque não funciona...
- *n.toString(91-b.charCodeAt()).replace(/[a-z]/,a=>String.fromCharCode(90-(a.charCodeAt()-97)))
- */
+*/
