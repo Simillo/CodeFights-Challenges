3a42c1dc2e06d23737163c9e07013746559a5d5a
Simillo <simillonakai@gmail.com>
2016-07-02 18:36:47 -0300

    54 chars & 32 chars

---
+++ BirdsLanguage.js
@@ -0,0 +1,2 @@
+//https://codefights.com/challenge/we7jqYhFEKNMmYxpN/main
+BirdsLanguage=w=>w.replace(/[aeiouAEIOU]/g,a=>a+'p'+a)
---
+++ FlipCup.js
@@ -0,0 +1,2 @@
+//https://codefights.com/challenge/kZJAxKBZkaWL7yGEt/main
+FlipCup=(n,f)=>(n/f%1?-1:n/f)+''
