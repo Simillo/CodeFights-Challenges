f23fab2c878948f293b7f031a12e3260c06ca0d2
Simillo <simillonakai@gmail.com>
2016-07-06 13:22:52 -0300

    -1 char

--- GenerateCode.js
+++ GenerateCode.js
@@ -1,30 +1,27 @@
 //https://codefights.com/challenge/GPw6LZT685xxJgnvT/main
 q = 'ABCDEFGHIJKL'
 GenerateCode = (l, y, m, n) =>
-eval(`
-    m--
-    s = l
-    y += ''
-    y = y.length < 2 ? '0' + y : y
-    p = y.length
-    for (i = p - 2; i < p; i++)
-        s += y[i]
-    s += q[m]
-    p = s.length
-    n += ''
-    l = n.length
-    if (p < 9)
-        if (l + p ^ 9) {
-            r = 9 - p
-            if (r > l) {
-                for (i = 0; i <= r - l - 1; i++)
-                    s += '0'
-                s + n
-            } else {
-                for (i = l - r; i <= l - 1; i++)
-                    s += n[i]
-                s
-            }
-        } else
+    eval(`
+        m--
+        s = l
+        y += ''
+        y = y.length < 2 ? '0' + y : y
+        p = y.length
+        for (i = p - 2; i < p; i++)
+            s += y[i]
+        s += q[m]
+        p = s.length
+        n += ''
+        l = n.length
+        if (p < 9)
+            if (l + p ^ 9)
+                if (r = 9 - p, r > l) {
+                    for (i = 0; i <= r - l - 1; i++) s += "0"
+                    s + n 
+                } else {
+                    for (i = l - r; i <= l - 1; i++) s += n[i]
+                    s
+                }
+        else
             s + n
-`) 
+    `) 
