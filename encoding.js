35fc8bb247ef20374ce665c55621dff7be7801a0
Simillo <simillonakai@gmail.com>
2016-07-10 01:46:29 -0300

    50 chars

---
+++ encoding.js
@@ -0,0 +1,7 @@
+//https://codefights.com/challenge/xmLcAnqyM2ozLB34y/main
+
+//NO CHEAT
+//encoding=n=>(''+n).replace(/./g,a=>String.fromCharCode(9-+a+97))
+
+//CHEAT
+encoding=n=>(''+n).replace(/./g,a=>' ihgfedcba'[a])
