2dbf4b49265f36bb454599b708f0b852a3c32324
Simillo <simillonakai@gmail.com>
2016-08-15 12:28:02 -0300

    195 chars

---
+++ emirpMinion.js
@@ -0,0 +1,17 @@
+//https://codefights.com/challenge/52cFreA4YEu3vmLu5/main
+emirpMinion=n=> {
+    n+=''
+    l=n.length
+    s=o=0
+    for(o;o<l;o++)
+        for(c=o;c<=l;c++)
+            p = n.substring(o,c),
+            s+=p-1&&f(p)&&f([...p].reverse().join``)?+p:0
+        
+    return !s?-1:s
+            
+}
+f=n=>{
+    for (i=2;i<=n/2;i++) if (n%i==0) return 0
+    return 1
+}
