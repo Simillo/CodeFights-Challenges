875ff580144df46682f1ec899536ab658b83155e
Simillo <simillonakai@gmail.com>
2016-08-22 20:28:31 -0300

    ---

--- ComputerUnitsToByte.js
+++ ComputerUnitsToByte.js
@@ -1,6 +1,2 @@
 //https://codefights.com/challenge/pYKuEoELw5sgo5oyM/main
-ComputerUnitsToByte=(c,u)=>{
-    for(i in p='KMGTP')
-        if(p[i]==u[0])
-            return Math.pow(2,(+i+1)*10)*c+''
-}
+ComputerUnitsToByte=(c,u)=>Math.pow(2,('KMGTP'.indexOf(u[0])+1)*10)*c+''
