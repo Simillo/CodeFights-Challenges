051a2ae65c1fc8855260ab185032b21f5c35056c
Simillo <simillonakai@gmail.com>
2016-10-30 19:48:58 -0200

    Primeiro code golf de cpp

---
+++ DigitSumRange.cpp
@@ -0,0 +1,12 @@
+//https://codefights.com/challenge/YeHucxXniMDZs2KAg
+int DigitSumRange(int a, int b) {
+    int s=0;
+    for(;a<=b;){
+        int c=a;
+        while(c){
+            s+=c%10;
+            c/=10;
+        }
+    }
+    return s;
+}
